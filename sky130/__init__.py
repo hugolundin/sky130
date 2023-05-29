e49ea8956ba48a39b9bf6dc7266684562959dc11
Hugo Lundin <hugo@lundin.dev>
2023-05-29 20:30:59 +0200

    Add spinit and prevent skidl from writing files

--- sky130/__init__.py
+++ sky130/__init__.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 
 import skidl
 from skidl.pyspice import *
@@ -7,10 +7,18 @@ from skidl.tools.spice import *
 SKY130_SPICE_LIB = '/opt/pdk/skywater-pdk/libraries/sky130_fd_pr/latest/models/sky130.lib.spice'
 SKY130_SPICE_CORNER = 'tt'
 
+# Prevent skidl from generating files when run. 
+skidl.no_files()
+
+# Ngspice looks for spinit here.
+os.environ["SPICE_SCRIPTS"] = os.path.dirname(os.path.abspath(__file__))
+
+# Disable warning about footprints not being found. 
+skidl.empty_footprint_handler = lambda _: _
+
 logging.disable()
 sky130 = SchLib(
     SKY130_SPICE_LIB,
     lib_section=SKY130_SPICE_CORNER,
     recurse=True)
-
 logging.disable(logging.NOTSET)
--- sky130/examples/current_mirror.py
+++ sky130/examples/current_mirror.py
@@ -6,17 +6,17 @@ from sky130.devices import nmos
 from sky130.sources import VDD, GND
 
 @subcircuit
-def current_mirror():
+def current_mirror(vdd, gnd):
     m1 = nmos(1)
 
+    gnd & m1.g
+    vdd & m1.d
+    gnd & m1.s
 
 if __name__ == "__main__":
-    cm = current_mirror()
-
-
-    #gnd = GND()
-    #vdd = VDD(5)
+    vdd = VDD(5)
+    gnd = GND()
+    cm = current_mirror(vdd, gnd)
 
     circuit = generate_netlist()
-    simulator = circuit.simulator()
     
